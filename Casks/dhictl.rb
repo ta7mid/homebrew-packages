9a038506d5937ded24da78b89400e9f1531021bf
Tahmid Khan <60913202+ta7mid@users.noreply.github.com>
2026-07-11 21:32:51 +0600

    dhictl 0.0.5: improve description

--- Casks/dhictl.rb
+++ Casks/dhictl.rb
@@ -2,15 +2,15 @@ cask "dhictl" do
   arch arm: "arm64", intel: "amd64"
   os macos: "darwin", linux: "linux"
 
-  version "0.0.3"
-  sha256 arm:          "e88ef3518d827eb2c529a09cbeff3e3243862c88a86cc97021178364bfa23a3a",
-         intel:        "beee713bcf54ef4e3a0195d576fb1c65798daceb50bf67d314ded9f38820b560",
-         arm64_linux:  "f81037f1efa2999bd15599079962cd0627e6211815c2335575ab7530573218f7",
-         x86_64_linux: "7bf49639777d9db7c937d4039db2b4e46de408936b03e9a38ae0ceee2e4cfad7"
+  version "0.0.5"
+  sha256 arm:          "460ec15d863188105de9e487b7905900d4d05d568609f52a7d67ce1e05f09b84",
+         intel:        "63fd1c9a4d15ee7e66a762f3bfec68775f875304555d78dff8ec8a764ceb1a5d",
+         arm64_linux:  "6cd76ef527f3f85fa3b27a0f60a602339cfa5d91b8b018174e9ca6a6615b638f",
+         x86_64_linux: "4f6da1177c6e4a8e41e25446bff53d04dd38dba8080ae7b6d5766569d1d4263c"
 
   url "https://github.com/docker-hardened-images/dhictl/releases/download/v#{version}/dhictl-#{os}-#{arch}"
   name "dhictl"
-  desc "Command-line tool for managing Docker Hardened Images"
+  desc "CLI tool for managing Docker Hardened Images (DHI)"
   homepage "https://github.com/docker-hardened-images/dhictl"
 
   binary "dhictl-#{os}-#{arch}", target: "dhictl"
